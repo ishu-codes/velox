322f0a62d8ce646b72855696e68ffdc9592de2f0
Ishu <kumarishu494@gmail.com>
2025-08-25 00:38:59 +0530

    Initialization

---
+++ server/src/main/java/ishu/velox/VeloxApplication.java
@@ -0,0 +1,13 @@
+package ishu.velox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeloxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeloxApplication.class, args);
+	}
+
+}
