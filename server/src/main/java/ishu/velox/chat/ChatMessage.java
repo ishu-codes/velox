8baac21de8e91780e6469177ffd665ff3f027b68
Ishu <kumarishu494@gmail.com>
2025-08-26 11:03:42 +0530

    id, timestamp attr added

--- server/src/main/java/ishu/velox/chat/ChatMessage.java
+++ server/src/main/java/ishu/velox/chat/ChatMessage.java
@@ -8,6 +8,7 @@ import lombok.*;
 @NoArgsConstructor
 @Builder
 public class ChatMessage {
+    private String id;
     private String content;
     private String sender;
     private MessageType type;
